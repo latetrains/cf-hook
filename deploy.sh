b1d7f178151e20a92ee14b9faa0086a73e964c32
Richard Hughes <richard.hughes@sykescottages.co.uk>
2019-02-22 14:36:29 +0000

    Add an update code function call

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ printf -- 'Building the ZIP file for upload\n\n';
 mkdir -p build
 zip -r build/src.zip src
 
-aws lambda update-function-code --function-name "$1" --zip-file build/src.zip
+aws lambda update-function-code --function-name "$1" --zip-file fileb://build/src.zip
 
 printf -- '\n\033[32mFinished \033[0m\n';
