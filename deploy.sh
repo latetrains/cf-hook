7652a71290c922aded4fe8685c0ae7b14905233a
Richie Hughes <eeuc40@hotmail.com>
2019-02-22 14:21:21 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ printf -- 'Building the ZIP file for upload\n\n';
 mkdir -p build
 zip -r build/src.zip src
 
+aws lambda update-function-code --function-name "$1" --zip-file build/src.zip
+
 printf -- '\n\033[32mFinished \033[0m\n';
